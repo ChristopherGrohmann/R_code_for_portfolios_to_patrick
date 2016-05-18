f89c9473e4acf1d9e6cd7b03c1b5775c9fcecbee
Chris Grohmann <cgrohmann@wiwi.uni-frankfurt.de>
2016-05-18 20:24:12 +0200

    works_but_2000 is not enough

--- Timeseries_for_mosttraded_instrumetns.R
+++ Timeseries_for_mosttraded_instrumetns.R
@@ -1,4 +1,10 @@
 #timeseries for most traded instruments
 
 bestinstr <- transactions[,.(number_of_trans = 0 ), by=.(instrument_id,date)]
-bestinstr[transactions[]]
+bestinstr[transactions[,.(number_of_trans = length(transaction_id)),by = .(instrument_id,date)], number_of_trans:=i.number_of_trans, on = "instrument_id"]
+bestinstr[transactions[,.(sumvalue = sum(value)),by = .(instrument_id,date)], sumvalue:=i.sumvalue, on = "instrument_id"]
+bestinstr[transactions[,instrument_description,by = .(instrument_id,date)], instrument_description:=i.instrument_description, on = "instrument_id"]
+bestinstr[bestinstr[,.(sumall = sum(sumvalue)),by =instrument_id], sumall:=i.sumall, on = "instrument_id"]
+bestinstr = bestinstr[order(-sumall,date)]
+
+write.csv(bestinstr[1:2000], file = "C:/Users/Grohmann/Documents/Interactive data/Descriptive/time_series_of_2000_stock_dates.csv")
