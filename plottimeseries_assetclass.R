502b9d942a050d10764ca64b24f0c66c2b8d0cb7
Chris Grohmann <cgrohmann@wiwi.uni-frankfurt.de>
2016-06-01 17:35:38 +0200

    extract the plotting to an extra function

---
+++ plottimeseries_assetclass.R
@@ -0,0 +1,16 @@
+plottimeseries_assetclass = function (x){
+  allplot  <- x[ ,.(plotvalue = mean(ValueStock),(Category="Stock") ), by = year]
+  allplot <- merge(allplot, x[ ,.(plotvalue = mean(ValueBond),(Category="Bond") ), by = year]
+                   ,by = c("year","V2", "plotvalue"), all = TRUE)
+  allplot <- merge(allplot, x[ ,.(plotvalue = mean(ValueFund),(Category="Fund") ), by = year]
+                   ,by = c("year","V2", "plotvalue"), all = TRUE)
+  allplot <- merge(allplot, x[ ,.(plotvalue = mean(ValueWarrent),(Category="Warrent") ), by = year]
+                   ,by = c("year","V2", "plotvalue"), all = TRUE)
+  allplot <- merge(allplot, x[ ,.(plotvalue = mean(ValueCertificate),(Category="Certificate") ), by = year]
+                   ,by = c("year","V2", "plotvalue"), all = TRUE)
+  allplot <- merge(allplot, x[ ,.(plotvalue = mean(ValueOther),(Category="Other") ), by = year]
+                   ,by = c("year","V2", "plotvalue"), all = TRUE)
+  ggplot(allplot, aes(x= year, y= plotvalue, group = V2, fill= plotvalue )) + geom_area(aes(fill= V2), position = 'stack') +
+    ggtitle( "Volume of transactions in asset class over time") +
+    theme(plot.title = element_text(lineheight=.8, face="bold"))
+}
